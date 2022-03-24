dee589aa7d651ad200ec4bf321c8a355be96a95c
Luan <luancianorte@gmail.com>
2022-03-23 22:20:44 -0300

    Fifth Commit

---
+++ Exercicio.Nove/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Nove
+{
+    internal struct Livro
+    {
+        public string Titulo { get; private set; } 
+
+        public string Autor { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public Livro(string titulo, string autor, decimal valor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Valor = valor;
+        }
+    }
+}
