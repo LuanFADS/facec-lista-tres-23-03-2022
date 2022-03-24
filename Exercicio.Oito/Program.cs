34dde00149a6b246130a830fde7b794e2a65d08b
Luan <luancianorte@gmail.com>
2022-03-23 21:40:36 -0300

    Second commit

--- Exercicio.Oito/Program.cs
+++ Exercicio.Oito/Program.cs
@@ -8,24 +8,39 @@ namespace Exercicio.Oito
 {
     internal class Program
     {
+        private readonly char[] _vogais = new char[5] {'a','e','i','o','u'};
+        
+        //private const char _a = 'a';
+        //private const char _e = 'e';
+        //private const char _i = 'i';
+        //private const char _o = 'o';
+        //private const char _u = 'u';
+
         static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine("Favor, digite o seu nome: ");  
+                Console.WriteLine("Favor, digite o seu nome: ");
                 var nome = Console.ReadLine().ToLower();
+                var count = 0;
 
-                for (int i = 0; i < nome.Length; i++)
-                {
-                    Console.WriteLine(nome[i]);
-                }
-
-                //op = char.Parse(Console.ReadLine());
-                //if (op != '+' || op != '-' || op != '*' || op != '/')
+                //if nome[] in _vogais[0..4]
                 //{
-                //    throw new Exception("Informe uma operação matemática de verdade entre [" +
-                //        " + | - | * | / ]");
+                    
                 //}
+
+                //for (int i = 0; i < nome.Length; i++)
+                //{
+                //    var letra = nome[i];
+                //    if (!_a.Equals(letra)
+                //        && !_e.Equals(letra)
+                //        && !_i.Equals(letra)
+                //        && !_o.Equals(letra)
+                //        && !_u.Equals(letra))
+                //        count++;
+                //}
+
+                Console.WriteLine($"\nO nome ({nome}) possui {count} consoantes!");
             }
             catch (Exception ex)
             {
